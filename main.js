549bea3e1f0ebb021c1ad16dfcaa9a938c7d81a4
eduardo <edmonteironet@gmail.com>
2022-06-18 00:55:30 -0300

    Tentando deixar o jogo orientado a objetos.

--- main.js
+++ main.js
@@ -1,96 +1,182 @@
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
-let box = 32;
-let snake = [];
+const TAMANHO_CAIXA = 32;
 
-let food = {
-  x: Math.floor(Math.random() * 15 + 1) * box,
-  y: Math.floor(Math.random() * 15 + 1) * box
-};
-
-let direction = 'right';
-
-snake[0] = {
-  x: 8 * box,
-  y: 8 * box
-};
-
-function criarBG() {
-  context.fillStyle = 'lightGreen';
-  context.fillRect(0, 0, 16 * box, 16 * box);
-}
-
-function criarCobrinha() {
-  for(let index = 0; index < snake.length; index++) {
-    context.fillStyle = 'green';
-    context.fillRect(snake[ index ].x, snake[ index ].y, box, box);
+class Ponto {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
 
-function drawFood() {
-  context.fillStyle = 'red';
-  context.fillRect(food.x, food.y, box, box);
+class Quadrado {
+  constructor(x, y, largura, altura, cor) {
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+    this.cor = cor;
+  }
+
+  desenhar(app) {
+    app.desenharQuadrado(this);
+  }
+
+  clonar() {
+    return new Quadrado(
+      this.x, this.y, 
+      this.largura, this.altura, 
+      this.cor);
+  }
 }
 
-document.addEventListener('keydown', function(event) {
-  if (event.keyCode == 37 && direction != 'right')  { direction = 'left'; }
-  if (event.keyCode == 38 && direction != 'down') { direction = 'up'; }
-  if (event.keyCode == 39 && direction != 'left') { direction = 'right'; }
-  if (event.keyCode == 40 && direction != 'up') { direction = 'down'; }
-});
-
-function iniciarJogo() {
-  if (snake[0].x > 15 * box && direction == 'right') {
-    snake[0].x = 0;
+class Comida extends Quadrado {
+  constructor() {
+    super(0, 0, 0, 0, 'red');
+    this.altura = TAMANHO_CAIXA;
+    this.largura = TAMANHO_CAIXA;
+    this.posicionar();
   }
 
-  if (snake[0].x < 0 && direction == 'left') {
-    snake[0].x = 16 * box;
+  posicionar() {
+    this.x = this.posicaoAleatoria();
+    this.y = this.posicaoAleatoria();
   }
 
-  if (snake[0].y > 15 * box && direction == 'down') {
-    snake[0].y = 0;
+  posicaoAleatoria() {
+    return Math.floor(Math.random() * 15 + 1) * TAMANHO_CAIXA;
+  }
+}
+
+class Cobra {
+  constructor(app) {
+    let posicao = 8 * TAMANHO_CAIXA/2;
+    this.direcao = 'right';
+    this.corpo = [ new Quadrado(posicao, posicao, TAMANHO_CAIXA, TAMANHO_CAIXA, 'green') ];
+    this.app = app;
   }
 
-  if (snake[0].y < 0 && direction == 'up') {
-    snake[0].y = 16 * box;
-  }
-  
-  for(let index = 1; index < snake.length; index++) {
-    if (snake[0].x == snake[index].x && snake[0].y == snake[index].y) {
-      clearInterval(jogo);
-      alert('Game Over :(');
+  orientarCabeca() {
+    let cabeca = this.corpo[ 0 ];
+    if ((cabeca.x > (15 * TAMANHO_CAIXA)) && (this.direcao == 'right')) {
+      cabeca.x = 0;
+    }
+
+    if ((cabeca.x < 0) && (this.direcao == 'left')) {
+      cabeca.x = 16 * TAMANHO_CAIXA;
+    }
+
+    if ((cabeca.y > (15 * TAMANHO_CAIXA)) && (this.direcao == 'down')) {
+      cabeca.y = 0;
+    }
+
+    if ((cabeca.y < 0) && (this.direcao == 'up')) {
+      cabeca.y = 16 * TAMANHO_CAIXA;
+    }
+
+    if (this.colisao()) {
+      this.app.stop();
     }
   }
 
-  criarBG();
-  criarCobrinha();
-  drawFood();
-
-  let snakex = snake[0].x;
-  let snakey = snake[0].y;
-
-  if (direction == 'right') snakex += box;
-  if (direction == 'left') snakex -= box;
-  if (direction == 'up') snakey -= box;
-  if (direction == 'down') snakey += box;
-
-  if (snakex != food.x || snakey != food.y) {
-    snake.pop();
-  }
-  else {
-    food.x = Math.floor(Math.random() * 15 + 1) * box;
-    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  mudarDirecao(code) {
+    if ((code == 37) && (this.direcao != 'right')) { this.direcao = 'left'; }
+    if ((code == 38) && (this.direcao != 'down')) { this.direcao = 'up'; }
+    if ((code == 39) && (this.direcao != 'left')) { this.direcao = 'right'; }
+    if ((code == 40) && (this.direcao != 'up')) { this.direcao = 'down'; }
   }
 
-  let head = {
-    x: snakex,
-    y: snakey
-  };
+  andar() {
+    let cabeca = this.corpo[ 0 ].clonar();
+    if (this.direcao == 'right') { cabeca.x += TAMANHO_CAIXA; }
+    if (this.direcao == 'left') { cabeca.x -= TAMANHO_CAIXA; }
+    if (this.direcao == 'up') { cabeca.y -= TAMANHO_CAIXA; }
+    if (this.direcao == 'down') { cabeca.y += TAMANHO_CAIXA; }
+    this.corpo.unshift(cabeca);
+  }
 
-  snake.unshift(head);  
+  comer(comida) {
+    let cabeca = this.corpo[ 0 ];
+
+    if ((cabeca.x != comida.x) || (cabeca.y != comida.y)) {
+      this.corpo.pop();
+    }
+    else {
+      comida.posicionar();
+    }
+  }
+
+  colisao() {
+    let cabeca = this.corpo[ 0 ];
+    let result = false;
+    for(let index = 1; index < this.corpo.length; index++) {
+      let cauda = this.corpo[ index ];
+      if ((cabeca.x == cauda.x) && (cabeca.y == cauda.y)) {
+        result = true;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  desenhar(comida) {
+    this.orientarCabeca();
+    for(let index = 0; index < this.corpo.length; index++) {
+      let quadrado = this.corpo[ index ];
+      quadrado.desenhar(this.app);
+    }
+
+    this.andar();
+    this.comer(comida);
+  }
 }
 
+class Tela {
+  constructor(app) {
+    this.area = new Quadrado(
+      0, 0, 
+      16 * TAMANHO_CAIXA, 
+      16 * TAMANHO_CAIXA, 
+      'lightGreen');
+    this.app = app;
+  }
 
+  desenhar() {
+    this.app.desenharQuadrado(this.area);
+  }
+}
 
-let jogo = setInterval(() => { iniciarJogo(); }, 100);
+class App {
+
+  constructor() {
+    this.canvas = document.getElementById('snake');
+    this.context = this.canvas.getContext('2d');
+    this.tela = new Tela(this);
+    this.cobra = new Cobra(this);
+    this.comida = new Comida();
+  }
+
+  start () {
+    document.addEventListener('keyup', (event) => {
+      this.cobra.mudarDirecao(event.keyCode);
+    });
+
+    this.appId = setInterval(() => {
+      this.tela.desenhar();
+      this.cobra.desenhar(this.comida);
+      this.comida.desenhar(this);
+    }, 100);
+  }
+
+  stop() {
+    clearInterval(this.appId);
+    alert('Game Over :(');
+  }
+
+  desenharQuadrado(quadrado) {
+    this.context.fillStyle = quadrado.cor;
+    this.context.fillRect(quadrado.x, quadrado.y, quadrado.largura, quadrado.altura);
+  }
+
+}
+
+new App().start();
